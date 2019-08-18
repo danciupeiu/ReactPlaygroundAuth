0211376c013e5c47b2c9c69a975b144f8a67b4cc
Dan Ciupeiu <ciupeiu_dan@yahoo.com>
2019-08-18 17:53:27 +0200

    Added client side authentication: added redux setup

---
+++ client/src/components/auth/SignUp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class SignUp extends React.Component {
+
+    render() {
+        return (
+            <form>
+                <fieldset>
+                    <label>Email</label>
+                </fieldset>
+                <fieldset>
+                    <label>Password</label>
+                </fieldset>
+            </form>
+        )
+    }
+}
+
+export default SignUp;
